98c78ba1065aaa2faf5b335c8df5fe32adb1790f
xatzos21 <xatzos21@gmail.com>
2022-08-17 15:30:12 +0200

    ex6

--- exercise.py
+++ exercise.py
@@ -234,8 +234,8 @@ x = int(input("First number: "))
 y = int(input("Second number: "))
 
 if x % y == 0:
-    print("First number is divisible by second number, result =", x // y)
+    print("First number is divisible by second number, result =", x / y)
 elif y % x == 0:
-    print("Second number is divisible by first number, result =", y // x)
+    print("Second number is divisible by first number, result =", y / x)
 else:
     print("Numbers are non-divisable!")
